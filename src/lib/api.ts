899e7a975dbfd894b88f92342fb8245f40fc02d6
Damir Lutfrakhmanov <60309177+DamirLut@users.noreply.github.com>
2022-09-09 21:58:36 +0500

    Опять фиксы

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -86,7 +86,7 @@ export class Api {
           Authorization: this.token,
         },
       })
-      .then((data) => {
+      .then(({ data }) => {
         return {
           data,
           cachedData: undefined,

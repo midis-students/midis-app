8051543f6853bb91e6a9cdf760545f0178828b38
DamirLut <d.amlutstudio@gmail.com>
2022-09-13 09:09:45 +0300

    Fix info atom

--- src/atoms/info.atom.ts
+++ src/atoms/info.atom.ts
@@ -1,10 +1,11 @@
 import { Api } from './../lib/api';
 import { selector } from 'recoil';
+import { ApiInfo } from '../lib/api.types';
 
 export const InfoAtom = selector({
   key: 'info.atom',
   get: async () => {
-    const cache = Api.getCache('info');
+    const cache = Api.getCache<ApiInfo>('info');
     if (cache) {
       return cache;
     }

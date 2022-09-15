bf434e596d913f4eaca594bb3c7bb386a43f2078
Damir Lutfrakhmanov <60309177+DamirLut@users.noreply.github.com>
2022-09-15 13:06:38 +0500

    Mark sorts

--- src/pages/Main/Panels/Marks/index.tsx
+++ src/pages/Main/Panels/Marks/index.tsx
@@ -6,22 +6,48 @@ import { Mark } from '../../../../lib/api.types';
 
 import style from './style.module.scss';
 
+type MarkExt = Mark & { object: string };
+
 export default function MarksView() {
   const marks = useRecoilValue(MarksAtom);
+
+  const transformed: Array<MarkExt> = [];
+  Object.entries(marks).forEach(([object, values]) => {
+    values.forEach((value) => {
+      transformed.push({
+        object,
+        ...value,
+      });
+    });
+  });
+
+  transformed.sort((prev, next) => {
+    const getTime = (value: string) => {
+      const [date, month, yaer] = value.split(' ')[1].split('.').map(Number);
+      const _date = new Date();
+      _date.setDate(date);
+      _date.setMonth(month);
+      _date.setFullYear(yaer);
+      return _date.getTime();
+    };
+    return getTime(next.date) - getTime(prev.date);
+  });
+
+  console.log(transformed);
 
   return (
     <View>
       <h1>Оценки</h1>
       <ul className={style['marks-list']}>
-        {Object.entries(marks).map(([object, values]) => (
-          <MarkView key={`${object}`} object={object} marks={values as Mark[]} />
+        {transformed.map((value) => (
+          <MarkView key={`${value.object}`} mark={value} />
         ))}
       </ul>
     </View>
   );
 }
 
-function MarkView({ marks, object }: { marks: Mark[]; object: string }) {
+function MarkView({ mark }: { mark: MarkExt }) {
   const markColor = (value: number) => {
     switch (value) {
       case 5:
@@ -37,19 +63,15 @@ function MarkView({ marks, object }: { marks: Mark[]; object: string }) {
   };
 
   return (
-    <>
-      {marks.map((mark) => (
-        <li key={mark.date + mark.details} className={style['mark-container']}>
-          <div className={style['mark-info']}>
-            <span>{object}</span>
-            <span>{mark.details}</span>
-            <span className={style['mark-date']}>{mark.date}</span>
-          </div>
-          <span className={style.mark} style={{ backgroundColor: markColor(+mark.mark) }}>
-            {mark.mark}
-          </span>
-        </li>
-      ))}
-    </>
+    <li key={mark.date + mark.details} className={style['mark-container']}>
+      <div className={style['mark-info']}>
+        <span>{mark.object}</span>
+        <span>{mark.details}</span>
+        <span className={style['mark-date']}>{mark.date}</span>
+      </div>
+      <span className={style.mark} style={{ backgroundColor: markColor(+mark.mark) }}>
+        {mark.mark}
+      </span>
+    </li>
   );
 }

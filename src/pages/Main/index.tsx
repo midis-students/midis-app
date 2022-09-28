b34e4edf17e10f4c11e50ec27adea6cb245ca398
Damir Lutfrakhmanov <60309177+DamirLut@users.noreply.github.com>
2022-09-28 13:06:29 +0500

    Fix start page

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -14,7 +14,7 @@ import ChatView from './Panels/Chat';
 import ObsidianView from './Panels/Obsidian';
 
 export default function MainPage() {
-  const [tabIndex, setTabIndex] = React.useState(2);
+  const [tabIndex, setTabIndex] = React.useState(1);
 
   const handelChangeIndex = (e: number) => {
     setTabIndex(e);

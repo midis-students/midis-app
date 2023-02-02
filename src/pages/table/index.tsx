a607ff8f61dacc6ed2680941d7da1ba449105f3d
Damir Lutfrakhmanov <60309177+DamirLut@users.noreply.github.com>
2023-02-02 11:19:39 +0500

    PWA

--- src/pages/table/index.tsx
+++ src/pages/table/index.tsx
@@ -12,6 +12,7 @@ export default function TablePage() {
 
   useEffect(() => {
     fetchSchedule();
+    document.title = "Расписание";
   }, []);
 
   if (!data) {
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      manifest: {
+        name: "Мидис Студент",
+        short_name: "midis-app",
+        start_url: ".",
+        display: "standalone",
+        description: "Мидис клиент для portal.midis.info",
+        background_color: "#f5f5f5",
+        theme_color: "#ff7c0a",
+        orientation: "portrait-primary",
+        icons: [48, 72, 96, 144, 168, 192].map((size) => ({
+          src: `icons/${size}.png`,
+          sizes: `${size}x${size}`,
+          type: "image/png",
+        })),
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": resolve("src"),
